c80454e58d07095ff21217ea27b0127f2f34b699
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-23 16:03:11 +0000

    wip

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -2,18 +2,29 @@
 
 namespace Aldrumo\Settings;
 
+use Contracts\Repository as SettingsContract;
 use Illuminate\Support\ServiceProvider;
 
 class SettingsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->registerRepository();
     }
 
     public function boot()
     {
         $this->bootMigrations();
+    }
+
+    protected function registerRepository()
+    {
+        $this->app->singleton(
+            SettingsContract::class,
+            function ($app) {
+                return new Repository();
+            }
+        );
     }
 
     protected function bootMigrations()
