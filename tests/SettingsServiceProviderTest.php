58345539868e7895167a98bc6eefba2af3d89efe
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-23 17:28:32 +0000

    wip

--- tests/SettingsServiceProviderTest.php
+++ tests/SettingsServiceProviderTest.php
@@ -2,11 +2,22 @@
 
 namespace Aldrumo\Settings\Tests;
 
+use Aldrumo\Settings\Contracts\Repository as SettingsContract;
+use Aldrumo\Settings\Repository;
+
 class SettingsServiceProviderTest extends TestCase
 {
     /** @test */
-    public function provider_is_loaded()
+    public function setting_repo_is_loaded()
     {
-        $this->assertTrue(true);
+        $this->assertInstanceOf(
+            Repository::class,
+            $this->app[SettingsContract::class]
+        );
+
+        $this->assertInstanceOf(
+            Repository::class,
+            app(SettingsContract::class)
+        );
     }
 }
